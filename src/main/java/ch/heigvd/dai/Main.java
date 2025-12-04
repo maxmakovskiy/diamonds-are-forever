78a91056e96b5739ed5c44c6b6181560528e5a06
maxmakovskiy <makovskiymax@gmail.com>
2025-12-04 14:31:13 +0100

    config: [init] adding javalin

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,7 +1,13 @@
 package ch.heigvd.dai;
 
+import io.javalin.Javalin;
+
 public class Main {
+    private static final int PORT = 8080;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Javalin app = Javalin.create();
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.start(PORT);
     }
 }
